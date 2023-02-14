9d840c53c098ddd8867d7f0e0334b79b0c60240b
amandamockaitis <amanda_mockaitis1@baylor.edu>
2023-02-14 15:12:00 -0600

    modified file

--- Student Object.py
+++ Student Object.py
@@ -4,7 +4,15 @@ import StudentClass as sc
 def main():
     sID = input("Enter student ID: ")
     name = input("Enter student name: ")
-    dob = int("Enter student date of birth (MM/DD/YYYY): ")
-    classif = "Enter student classification (F,S,Jr,Sr): "
+    dob = input("Enter student date of birth (MM/DD/YYYY): ")
+    classif = input("Enter student classification (F,S,Jr,Sr): ")
 
     student_1 = sc.Student(sID, name, dob, classif)
+
+    student_1.age_calc()
+
+    student_1.register()
+
+    print("Student age is:", student_1.get_age())
+
+    print("Student can reigster between:", student_1.return_registration())
--- StudentClass.py
+++ StudentClass.py
@@ -4,14 +4,25 @@ from datetime import date
 class Student:
     def __init__(self, sID, name, dob, classif):
         self.__stuID = sID
-        self.name = name
-        self.dateofbirth = dob
-        self.classification = classif
-        self.age = 0
+        self.__name = name
+        self.__dob = dob
+        self.__classification = classif
+        self.__age = 0
+        self.__register = ""
 
     def age_calc(self, dob):
         today = date.today()
-        self.age = today - dob
+        dob = self.__dob.split("/")
+        dob_year = int(dob[2])
+        age = today.year - dob_year
+        self.__age = age
+
+    def register(self):
+        # bunch of if statements
+        print()
 
     def get_age(self):
-        return self.age
+        return self.__age
+
+    def return_registration(self):
+        return self.__register
